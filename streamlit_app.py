0b878c4905f83432511e3278a2ad6ca2b14ea330
emlittle <161651900+emlittle@users.noreply.github.com>
2024-11-22 09:39:14 +1300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,9 +18,6 @@ cursor.execute("SELECT fruit_name, search_on FROM fruit_options order by fruit_i
 my_dataframe = pd.DataFrame(cursor.fetchall(), columns=['FRUIT_NAME', 'SEARCH_ON'])
 st.write(my_dataframe)
 
-#my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-#st.dataframe(data=my_dataframe, use_container_width=True)
-
 name_on_order = st.text_input('Name on Smoothie:')
 st.write('The name on your smoothie will be:', name_on_order)
 ingredients_list = st.multiselect('Choose up to 5 ingredients:', my_dataframe['FRUIT_NAME'], max_selections=5)
@@ -32,10 +29,9 @@ if ingredients_list:
         ingredients_string += fruit_chosen + ' '
         
         search_on=my_dataframe.loc[my_dataframe['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
         
         st.subheader(fruit_chosen + ' Nutrition Information')
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + search_on)
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
